18c54e50242db448813181c28fc99d105f6f6138
max <maximilian.kernbach@gmail.com>
2022-05-11 11:23:00 +0200

    add cugraph functionality

--- adbcug_adapter/adapter.py
+++ adbcug_adapter/adapter.py
@@ -40,6 +40,124 @@ class ADBCUG_Adapter(Abstract_ADBCUG_Adapter):
         print(f"Connecting to {url}")
         self.__db = ArangoClient(hosts=url).db(db_name, username, password, verify=True)
 
+    def arangodb_to_cugraph(
+        self,
+        name: str,
+        metagraph: ArangoMetagraph,
+        is_keep: bool = True,
+        **query_options: Any,
+    ) -> cuGraphMultiGraph(directed=True):  # type: ignore
+        """Create a cuGraph graph from graph attributes.
+        :param name: The cuGraph graph name.
+        :type name: str
+        :param metagraph: An object defining vertex & edge collections to import to
+            cuGraph, along with their associated attributes to keep.
+        :type metagraph: adbcug_adapter.typings.ArangoMetagraph
+        :param is_keep: Only keep the document attributes specified in **metagraph**
+            when importing to cuGraph (is True by default).
+        :type is_keep: bool
+        :param query_options: Keyword arguments to specify AQL query options when
+            fetching documents from the ArangoDB instance.
+        :type query_options: Any
+        :return: A Multi-Directed cuGraph Graph.
+        :rtype: cugraph.structure.graph_classes.MultiDiGraph
+        :raise ValueError: If missing required keys in metagraph
+        Here is an example entry for parameter **metagraph**:
+        .. code-block:: python
+        {
+            "vertexCollections": {
+                "account": {"Balance", "account_type", "customer_id", "rank"},
+                "bank": {"Country", "Id", "bank_id", "bank_name"},
+                "customer": {"Name", "Sex", "Ssn", "rank"},
+            },
+            "edgeCollections": {
+                "accountHolder": {},
+                "transaction": {
+                    "transaction_amt", "receiver_bank_id", "sender_bank_id"
+                },
+            },
+        }
+        """
+        self.__validate_attributes("graph", set(metagraph), self.METAGRAPH_ATRIBS)
+
+        # Maps ArangoDB vertex IDs to cuGraph node IDs
+        adb_map: Dict[str, Dict[str, Union[CuGId, str]]] = dict()
+        cg_edges: List[Tuple[CuGId, CuGId]] = []
+
+        adb_v: Json
+        for col, atribs in metagraph["vertexCollections"].items():
+            for adb_v in self.__fetch_adb_docs(col, atribs, is_keep, query_options):
+                adb_id: str = adb_v["_id"]
+                nx_id = self.__cntrl._prepare_arangodb_vertex(adb_v, col)
+                adb_map[adb_id] = {"nx_id": nx_id, "collection": col}
+
+        adb_e: Json
+        for col, atribs in metagraph["edgeCollections"].items():
+            for adb_e in self.__fetch_adb_docs(col, atribs, is_keep, query_options):
+                from_node_id: CuGId = adb_map[adb_e["_from"]]["nx_id"]
+                to_node_id: CuGId = adb_map[adb_e["_to"]]["nx_id"]
+                self.__cntrl._prepare_arangodb_edge(adb_e, col)
+                cg_edges.append((from_node_id, to_node_id))
+
+        srcs = [s for (s, _) in cg_edges]
+        dsts = [d for (_, d) in cg_edges]
+        cg_graph = cuGraphMultiGraph(directed=True)
+        cg_graph.from_cudf_edgelist(
+            DataFrame({"source": srcs, "destination": dsts})
+        )
+
+        print(f"cuGraph: {name} created")
+        return cg_graph
+
+    def arangodb_collections_to_cugraph(
+        self,
+        name: str,
+        v_cols: Set[str],
+        e_cols: Set[str],
+        **query_options: Any,
+    ) -> cuGraphMultiGraph(directed=True):  # type: ignore
+        """Create a cuGraph graph from ArangoDB collections.
+        :param name: The cuGraph graph name.
+        :type name: str
+        :param v_cols: A set of vertex collections to import to cuGraph.
+        :type v_cols: Set[str]
+        :param e_cols: A set of edge collections to import to cuGraph.
+        :type e_cols: Set[str]
+        :param query_options: Keyword arguments to specify AQL query options when
+            fetching documents from the ArangoDB instance.
+        :type query_options: Any
+        :return: A Multi-Directed cuGraph Graph.
+        :rtype: cugraph.structure.graph_classes.MultiDiGraph
+        """
+        metagraph: ArangoMetagraph = {
+            "vertexCollections": {col: set() for col in v_cols},
+            "edgeCollections": {col: set() for col in e_cols},
+        }
+
+        return self.arangodb_to_cugraph(
+            name, metagraph, is_keep=True, **query_options
+        )
+
+    def arangodb_graph_to_cugraph(
+        self, name: str, **query_options: Any
+    ) -> cuGraphMultiGraph(directed=True):  # type: ignore
+        """Create a cuGraph graph from an ArangoDB graph.
+        :param name: The ArangoDB graph name.
+        :type name: str
+        :param query_options: Keyword arguments to specify AQL query options when
+            fetching documents from the ArangoDB instance.
+        :type query_options: Any
+        :return: A Multi-Directed cuGraph Graph.
+        :rtype: cugraph.structure.graph_classes.MultiDiGraph
+        """
+        graph = self.__db.graph(name)
+        v_cols = graph.vertex_collections()
+        e_cols = {col["edge_collection"] for col in graph.edge_definitions()}
+
+        return self.arangodb_collections_to_cugraph(
+            name, v_cols, e_cols, **query_options
+        )
+
     def __validate_attributes(
         self, type: str, attributes: Set[str], valid_attributes: Set[str]
     ) -> None:
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import io
 import json
 import os
 import subprocess
+import time
 import urllib.request as urllib
 import zipfile
 from pathlib import Path
@@ -9,9 +10,10 @@ from typing import Any
 
 from arango import ArangoClient
 from arango.database import StandardDatabase
+from requests import post
 
 from adbcug_adapter.adapter import ADBCUG_Adapter
-from adbcug_adapter.typings import Json, NxData, NxId
+from adbcug_adapter.typings import Json, CuGId
 
 PROJECT_DIR = Path(__file__).parent.parent
 
@@ -19,47 +21,31 @@ con: Json
 adbcug_adapter: ADBCUG_Adapter
 db: StandardDatabase
 
-
-def pytest_addoption(parser: Any) -> None:
-    parser.addoption("--protocol", action="store", default="http")
-    parser.addoption("--host", action="store", default="localhost")
-    parser.addoption("--port", action="store", default="8529")
-    parser.addoption("--dbName", action="store", default="_system")
-    parser.addoption("--username", action="store", default="root")
-    parser.addoption("--password", action="store", default="openSesame")
-
-
-def pytest_configure(config) -> None:
+def pytest_sessionstart() -> None:
     global con
-    con = {
-        "protocol": config.getoption("protocol"),
-        "hostname": config.getoption("host"),
-        "port": config.getoption("port"),
-        "username": config.getoption("username"),
-        "password": config.getoption("password"),
-        "dbName": config.getoption("dbName"),
-    }
-
-    print("----------------------------------------")
-    print(f"{con['protocol']}://{con['hostname']}:{con['port']}")
-    print("Username: " + con["username"])
-    print("Password: " + con["password"])
-    print("Database: " + con["dbName"])
-    print("----------------------------------------")
+    con = get_oasis_crendetials()
+    # con = {
+    #     "username": "root",
+    #     "password": "openSesame",
+    #     "hostname": "localhost",
+    #     "port": 8529,
+    #     "protocol": "http",
+    #     "dbName": "_system",
+    # }
+    print_connection_details(con)
+    time.sleep(5)  # Enough for the oasis instance to be ready.
 
     global adbcug_adapter
-    adbcug_adapter = ADBCUG_Adapter(con)
+    adcug_adapter = ADBCUG_Adapter(con)
 
     global db
-    url = con["protocol"] + "://" + con["hostname"] + ":" + str(con["port"])
+    url = "https://" + con["hostname"] + ":" + str(con["port"])
     client = ArangoClient(hosts=url)
     db = client.db(con["dbName"], con["username"], con["password"], verify=True)
 
     arango_restore(con, "examples/data/fraud_dump")
     arango_restore(con, "examples/data/imdb_dump")
 
-    # Create Fraud Detection Graph
-    db.delete_graph("fraud-detection", ignore_missing=True)
     db.create_graph(
         "fraud-detection",
         edge_definitions=[
@@ -76,6 +62,22 @@ def pytest_configure(config) -> None:
         ],
     )
 
+def get_oasis_crendetials() -> Json:
+    url = "https://tutorials.arangodb.cloud:8529/_db/_system/tutorialDB/tutorialDB"
+    request = post(url, data=json.dumps("{}"))
+    if request.status_code != 200:
+        raise Exception("Error retrieving login data.")
+
+    creds: Json = json.loads(request.text)
+    return creds
+
+def print_connection_details(con: Json) -> None:
+    print("----------------------------------------")
+    print("https://{}:{}".format(con["hostname"], con["port"]))
+    print("Username: " + con["username"])
+    print("Password: " + con["password"])
+    print("Database: " + con["dbName"])
+    print("----------------------------------------")
 
 def arango_restore(con: Json, path_to_data: str) -> None:
     restore_prefix = "./assets/" if os.getenv("GITHUB_ACTIONS") else ""
